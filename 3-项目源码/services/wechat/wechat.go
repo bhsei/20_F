e6649be5e1b02c959efec0b797cb6d2b5d18c2fe
wangweimin <wang0.618@qq.com>
2020-04-12 17:15:53 +0800

    feature: 实现微信模块公共接口

--- 3-项目源码/services/wechat/wechat.go
+++ 3-项目源码/services/wechat/wechat.go
@@ -1,13 +1,33 @@
 package wechat
 
-import (
-	"code.gitea.io/gitea/modules/setting"
-)
+import "code.gitea.io/gitea/modules/setting"
 
+type Message struct {
+	openid  string
+	content string
+	url     string
+}
+
+// SendWechatMsg 将消息放到发送队列中，异步发送微信通知
 func SendWechatMsg(message, openid, url string) bool {
+	msg := &Message{
+		openid:  openid,
+		content: message,
+		url:     url,
+	}
+
+	go func() {
+		messageQueue <- msg
+	}()
+
 	return true
 }
 
+// GetQRCode 生成扫描后重定向到服务号的二维码, 返回二维码的内容
+// data 为二维码携带的数据，微信用户扫描二维码后微信会将扫描事件和二维码携带的数据发送给Gitea服务器
+// expireSeconds 为二维码有效期
 func GetQRCode(data string, expireSeconds int) string {
-	return ""
+	token := getCachedToken(setting.Wechat.AppId, setting.Wechat.AppSecret)
+	qrLink := getQRCode(token, data, expireSeconds)
+	return qrLink
 }
