974378647fe899d94f2800c8b8f3e8659a0d59a7
wangweimin <wang0.618@qq.com>
2020-04-12 16:48:12 +0800

    test: 添加微信服务号测试用例

---
+++ 3-项目源码/services/wechat/api_test.go
@@ -0,0 +1,43 @@
+package wechat
+
+import (
+	"os"
+	"testing"
+	"time"
+)
+
+var appID, appSecret, openID string
+
+func init() {
+	appID = os.Getenv("APPID")
+	appSecret = os.Getenv("APPSECRET")
+	openID = os.Getenv("OPENID")
+}
+
+func TestGetQRCode(t *testing.T) {
+	t.Logf("Use appID:%s appSecret:%s", appID, appSecret)
+	token, _ := getToken(appID, appSecret)
+	t.Logf("Use access token:%.10s...", token)
+
+	qrStr := getQRCode(token, "test", 3600)
+	t.Logf("getQRCode -> %s", qrStr)
+}
+
+func TestSendWechatMsg(t *testing.T) {
+	t.Logf("Use appID:%s appSecret:%s", appID, appSecret)
+	token, _ := getToken(appID, appSecret)
+	t.Logf("Use access token:%.10s...", token)
+
+	sendMessage(token, openID, "Hello", "https://baidu.com")
+	t.Logf("sendMessage to %s", openID)
+}
+
+func TestGetCachedToken(t *testing.T) {
+	t.Logf("Use appID:%s appSecret:%s", appID, appSecret)
+	token := getCachedToken(appID, appSecret)
+	t.Logf("Sleep some seconds...")
+	time.Sleep(8 * time.Second)
+	if token != getCachedToken(appID, appSecret) {
+		t.Errorf("两次获取到的缓存不一致")
+	}
+}
